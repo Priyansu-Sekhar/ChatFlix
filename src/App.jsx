a840b8298a5b496cb7b0040edd525b6508e20431
Priyansu-Sekhar <priyansusekhar09@gmail.com>
2026-04-09 11:17:12 +0530

    Project Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ChatPage from './pages/ChatPage.jsx'
+import AboutPage from './pages/AboutPage'
+import EntryPage from './pages/EntryPage'
+import Contact from './components/Contact'
+import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+     <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ChatPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/entry" element={<EntryPage />} />
+          
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
